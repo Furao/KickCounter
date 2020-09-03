7eb2d5f5d74c669af94c067b2b6060d274c0936a
Furao <vanvossr@gmail.com>
2020-09-03 14:35:32 -0400

    Make significant improvements
    App starts a session for counting now. Keeps track of state
    persistently. Made lots of fixes to logic as well.

--- App.tsx
+++ App.tsx
@@ -1,86 +1,82 @@
-// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert, Dimensions, StatusBar} from 'react-native';
-// import { BarChart} from "react-native-chart-kit";
-// import Moment from "react-moment";
-import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryLabel } from "victory-native";
 import moment from 'moment';
+import * as SplashScreen from 'expo-splash-screen';
 
 const screenWidth = Dimensions.get("window").width;
-
-// Moment.globalFormat = 'D MMM YYYY';
 
 var Datastore = require('react-native-local-mongodb')
   , db = new Datastore({ filename: 'asyncStorageKey', autoload: true });
 
-db.persistence.setAutocompactionInterval(1000)
+// db.persistence.setAutocompactionInterval(1000)
 
-function checkDB() {
-  let num = db.count({}, function (err, count) {
-    var i = 0;
-    console.log(count);
-    var today = new Date();
-    today.setMilliseconds(0);
-    today.setSeconds(0);
-    today.setMinutes(0);
-    if(count == 0) {
-      console.log("Filling Database");
-      db.insert({date: today, kicks: 0});
-      for (i = 0; i < 720; i++) {
-        db.insert({date: today, kicks: 0});
-        today = new Date(today-3600000);
-      }
-    }
-    else {
-      db.find({}).sort({ date: -1 }).limit(1).exec(function (err, docs) {
-        let latest_hour = docs[0].date
-        console.log(latest_hour);
-        console.log(today);
-        if(latest_hour.getTime() != today.getTime()) {
-          console.log("Latest hour is not now");
-          let hours_between=(today-latest_hour)/3600000;
+// function checkDB() {
+//   let num = db.count({}, function (err, count) {
+//     var i = 0;
+//     console.log(count);
+//     var today = new Date();
+//     today.setMilliseconds(0);
+//     today.setSeconds(0);
+//     today.setMinutes(0);
+//     if(count == 0) {
+//       console.log("Filling Database");
+//       db.insert({date: today, kicks: 0});
+//       for (i = 0; i < 720; i++) {
+//         db.insert({date: today, kicks: 0});
+//         today = new Date(today-3600000);
+//       }
+//     }
+//     else {
+//       db.find({}).sort({ date: -1 }).limit(1).exec(function (err, docs) {
+//         let latest_hour = docs[0].date
+//         console.log(latest_hour);
+//         console.log(today);
+//         if(latest_hour.getTime() != today.getTime()) {
+//           console.log("Latest hour is not now");
+//           let hours_between=(today-latest_hour)/3600000;
+//
+//           for(i = 0; i<(hours_between);i++){
+//             console.log("adding empty hour");
+//             latest_hour = new Date(latest_hour+3600000);
+//             db.insert({date: latest_hour, kicks: 0});
+//           }
+//         }
+//       });
+//     }
+//   });
+// }
 
-          for(i = 0; i<(hours_between);i++){
-            console.log("adding empty hour");
-            latest_hour = new Date(latest_hour+3600000);
-            db.insert({date: latest_hour, kicks: 0});
-          }
-        }
-      });
-    }
-  });
-}
-
-function kicked() {
-  var today = new Date();
-  today.setMilliseconds(0);
-  today.setSeconds(0);
-  today.setMinutes(0);
-  // var curr_time = <Moment element={Text}>today</Moment>;
-  db.find({date: today}, function (err, docs) {
-
-    if(docs.length == 0) {
-      db.find({}).sort({ date: -1 }).limit(1).exec(function (err, docs) {
-        let latest_hour = docs[0].date
-        let hours_between=(today-latest_hour)/3600000;
-
-        var i = 0;
-        for(i = 0; i<(hours_between-1);i++){
-          latest_hour = new Date(latest_hour+3600000);
-          db.insert({date: latest_hour, kicks: 0});
-        }
-        console.log("No kicks logged at " + today);
-        db.insert({kicks: 1, date: today});
-      });
-    }
-    else {
-      db.update({date: today}, { $inc: { kicks: 1 } }, {});
-    }
-  })
-
-  Alert.alert("Added a baby kick")
-
-}
+// function kicked() {
+//   var today = new Date();
+//   today.setMilliseconds(0);
+//   today.setSeconds(0);
+//   today.setMinutes(0);
+//   // var curr_time = <Moment element={Text}>today</Moment>;
+//   db.find({date: today}, function (err, docs) {
+//
+//     if(docs.length == 0) {
+//       db.find({}).sort({ date: -1 }).limit(1).exec(function (err, docs) {
+//         let latest_hour = docs[0].date
+//         let hours_between=(today-latest_hour)/3600000;
+//
+//         var i = 0;
+//         for(i = 0; i<(hours_between-1);i++){
+//           latest_hour = new Date(latest_hour+3600000);
+//           db.insert({date: latest_hour, kicks: 0});
+//         }
+//         console.log("No kicks logged at " + today);
+//         db.insert({kicks: 1, date: today});
+//       });
+//     }
+//     else {
+//       db.update({date: today}, { $inc: { kicks: 1 } }, {});
+//     }
+//   })
+//
+//   Alert.alert("Added a baby kick")
+//
+// }
 
 
 export default class App extends React.Component {
@@ -90,16 +86,103 @@ export default class App extends React.Component {
     this.state = {
       data: [{date:"0", kicks: 0},
               {date:"0", kicks: 0}],
-      skip: 0
+      skip: 0,
+      entries: 0,
+      counting: false,
+      kickButton_string: "Start Counting Kicks",
+      appIsReady: false
     };
-    this.getData();
-    checkDB();
+    // this.getData();
+    this.hourFinished = this.hourFinished.bind(this);
+  }
+
+  async checkDB() {
+    var obj = this;
+    await db.find({}).sort({ date: -1 }).limit(1).exec(function (err, docs) {
+      if(docs.length) {
+        db.count({}, function (err, count) {
+          obj.setState({
+            entries: count
+          });
+        });
+        if(docs[0].active == 1){
+          let start_time = docs[0].date;
+          var curr_time = new Date();
+          if(curr_time-start_time > 3*1000)
+          {
+            obj.hourFinished(docs[0]._id);
+          }
+          else
+          {
+            obj.setState({
+              counting: true
+            });
+            obj.setState({
+              kickButton_string: "Baby Kicked"
+            });
+          }
+
+        }
+      }
+    });
+    this.setState({ appIsReady: true }, async () => {
+      await SplashScreen.hideAsync();
+    });
+  }
+
+  hourFinished(id) {
+    Alert.alert("Finished hour for counting")
+    var obj = this;
+    db.update({ _id: id }, { $set: { active: 0 } }, {}, function () {
+      db.persistence.compactDatafile();
+      obj.setState({
+        kickButton_string: "Start Counting Kicks"
+      });
+      obj.setState({
+        counting: false
+      });
+    });
+  }
+
+  onKicked() {
+    var obj = this;
+    if(this.state.counting)
+    {
+      db.find({}).sort({ date: -1 }).limit(1).exec(function (err, docs) {
+        console.log(docs);
+        if(docs[0].active == 1){
+          console.log("Still counting kicks");
+          db.update({ _id: docs[0]._id }, { $push: { kicks: new Date() } }, {}, function () {
+            db.persistence.compactDatafile();
+          });
+
+        }
+      });
+    }
+    else
+    {
+      var curr_time = new Date();
+      db.insert({date: curr_time, active: 1, kicks: []}, function (err, newDoc) {
+        window.setTimeout(obj.hourFinished, 3*1000, newDoc._id);
+        obj.setState({
+          entries: obj.state.entries+1
+        });
+        db.persistence.compactDatafile();
+      });
+      console.log("Start Counting Kicks");
+      this.setState({
+        kickButton_string: "Baby Kicked"
+      });
+      this.setState({
+        counting: true
+      });
+    }
+
   }
 
   onLeft() {
-    console.log("onLeft");
+    var obj = this;
     let cur_val = this.state.skip+4;
-    console.log("new skip: " + cur_val);
     this.setState({
       skip: cur_val
     });
@@ -112,15 +195,18 @@ export default class App extends React.Component {
     });
   }
 
-  componentDidMount() {
-      this.setStateInterval = window.setInterval(() => {
-        this.getData();
-        // console.log(newData);
-        // this.setState({
-        //   data: newData
-        // });
-      }, 250);
+  async componentDidMount() {
+    // Prevent native splash screen from autohiding
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
     }
+    this.checkDB();
+    this.setStateInterval = window.setInterval(() => {
+        this.getData();
+    }, 250);
+  }
 
     componentWillUnmount() {
       window.clearInterval(this.setStateInterval);
@@ -130,13 +216,26 @@ export default class App extends React.Component {
 // {(d) => moment(d.date).format('MM/DD ha')}
     getData() {
       var obj = this;
-      db.find({}).sort({ date: -1 }).skip(this.state.skip).limit(4).exec(function (err, docs) {
+      db.find({}).sort({ date: -1 }).limit(4).skip(this.state.skip).exec(function (err, docs) {
         // console.log(docs);
         // data = docs
+        const new_data = docs.map((x) => {
+          return {date: x.date, kicks: x.kicks.length};
+        });
         obj.setState({
-          data: docs
+          data: new_data
         });
       });
+      if(this.state.counting==true) {
+        this.setState({
+          kickButton_string: "Baby Kicked"
+        });
+      }
+      else {
+        this.setState({
+          kickButton_string: "Start Counting Kicks"
+        });
+      }
 
     // const data = [
     //   { date: "08/24\r\n1pm", kicks: 10 },
@@ -162,6 +261,9 @@ export default class App extends React.Component {
   }
 
   render() {
+    if (!this.state.appIsReady) {
+      return null;
+    }
     return (
       <View style={styles.container}>
         <StatusBar style="auto" />
@@ -169,14 +271,15 @@ export default class App extends React.Component {
 
         <View style = {styles.actionBox} >
           <Button
-            onPress={kicked}
-            title="Baby Kicked"
+            onPress={this.onKicked.bind(this)}
+            title={this.state.kickButton_string}
             color="#2eb872"
           />
         </View>
         <View style = {styles.flexRow} >
           <Button
             onPress={this.onLeft.bind(this)}
+            disabled={this.state.entries<=this.state.skip+4}
             title="Left"
             color="#2eb872"
           />
@@ -188,8 +291,22 @@ export default class App extends React.Component {
           />
         </View>
         <View style = {styles.graphView} >
-          <VictoryChart width={screenWidth} theme={VictoryTheme.material}>
-              <VictoryBar domain={{y: [0, 40]}} alignment="start" data={this.state.data} sortKey="date" x={(d) => moment(d.date).format("MM/DD") + "\r\n"+ moment(d.date).format("h a")} y="kicks" />
+          <VictoryChart domainPadding={30} width={screenWidth} theme={VictoryTheme.material}>
+              <VictoryBar
+                domain={{y: [0, 40]}}
+                // alignment="start"
+                data={this.state.data}
+                sortKey="date"
+                x={(d) => moment(d.date).format("MM/DD") + "\r\n"+ moment(d.date).format("h:mm:ss a")}
+                y="kicks"
+                labels={({ datum }) => `${datum.kicks}`}
+                barWidth={30}
+                labelComponent={
+                  <VictoryLabel
+                    textAnchor={({ text }) => "middle"}
+                  />
+                }
+              />
             </VictoryChart>
         </View>
         <View style = {{height:30}} />
